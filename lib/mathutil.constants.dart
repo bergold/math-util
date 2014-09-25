c4301aad266554a40330191d4a6541cbc06a810b
bergold <emil.bergold@outlook.com>
2014-09-25 20:45:43 +0200

    add PHI and RANDOM constant

--- lib/mathutil.constants.dart
+++ lib/mathutil.constants.dart
@@ -1,6 +1,6 @@
 library mathutil.constants;
 
-import 'dart:math' show PI, E;
+import 'dart:math' show PI, E, Random, sqrt;
 
 /// This class represents a mathematical constant.
 class Constant {
@@ -14,8 +14,12 @@ class Constant {
 }
 
 
+var _rand = new Random();
+
 /// A map of mathematical constants.
 Map<String, Constant> constants = {
-  'PI': new Constant(() => PI),
-  'E': new Constant(() => E)
+  "PI": new Constant(() => PI),
+  "E": new Constant(() => E),
+  "PHI": new Constant(() => (1 + sqrt(5)) / 2),
+  "RANDOM": new Constant(() => _rand.nextDouble())
 };
