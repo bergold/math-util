72bbf10604cf98c8b55a6d20f696b563a8050cdd
bergold <emil.bergold@outlook.com>
2014-09-25 20:22:47 +0200

    implement operators

--- lib/mathutil.operators.dart
+++ lib/mathutil.operators.dart
@@ -1,13 +1,16 @@
 library mathutil.operators.dart;
 
+import 'dart:math' as math;
+
 /// This class represents a mathematical operator.
 class Operator {
+  final String symbol;
   final String assoc;
   final int precedence;
   final int operands;
   Function _fn;
   
-  Operator(this.assoc, this.precedence, this.operands, this._fn) {
+  Operator(this.symbol, this.assoc, this.precedence, this.operands, this._fn) {
     if (assoc != "left" && assoc != "right") throw new Exception("Assoc must be 'left' or 'right'.");
   }
   
@@ -20,7 +23,19 @@ class Operator {
 }
 
 
-/// A map of mathematical operators.
-Map<String, Operator> operators = {
-  "+": new Operator("left", 1, 2, (a, b) => a + b)
-};
+/// A List of mathematical operators.
+List<Operator> operators = [
+  new Operator("+", "left", 1, 2, (a, b) => a + b),
+  new Operator("-", "left", 1, 2, (a, b) => a - b),
+  
+  new Operator("*", "left", 2, 2, (a, b) => a * b),
+  new Operator("/", "left", 2, 2, (a, b) => a / b),
+  new Operator("mod", "left", 2, 2, (a, b) => a % b),
+  
+  new Operator("-", "left", 3, 1, (a) => -a),
+    
+  new Operator("^", "right", 4, 2, (a, b) => math.pow(a, b)),
+    
+  new Operator("%", "right", 5, 1, (a) => a / 100),
+  new Operator("!", "right", 5, 1, (a) => a) // [Todo] Implement factorial operator.
+];
