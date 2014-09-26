f23c9949f427f2ba8f1ff7094226925dc0955f55
bergold <emil.bergold@outlook.com>
2014-09-26 17:39:20 +0200

    add tests for angleunits

--- test/angleunits_test.dart
+++ test/angleunits_test.dart
@@ -1,12 +1,29 @@
 library mathutil.test.angleunits;
 
+import 'dart:math' show PI;
+
 import 'package:unittest/unittest.dart';
 import 'package:mathutil/mathutil.dart';
 
 void run() {
   group("Angleunits", () {
     test("degtorad", () {
+      expect(degtorad(180), equals(PI));
+    });
+    test("degtograd", () {
       expect(degtograd(180), equals(200));
+    });
+    test("radtodeg", () {
+      expect(radtodeg(PI), equals(180));
+    });
+    test("radtograd", () {
+      expect(radtograd(PI), equals(200));
+    });
+    test("gradtodeg", () {
+      expect(gradtodeg(200), equals(180));
+    });
+    test("gradtorad", () {
+      expect(gradtorad(200), equals(PI));
     });
   });
 }
